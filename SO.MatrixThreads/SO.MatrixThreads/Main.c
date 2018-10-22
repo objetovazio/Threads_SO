ddf02f6effb5682abc2cdee87b87b4acbea6d47a
André Martins <objetovazio@gmail.com>
2018-10-22 09:41:22 -0300

    Commit teste
    comitei pra testar

--- SO.MatrixThreads/SO.MatrixThreads/Main.c
+++ SO.MatrixThreads/SO.MatrixThreads/Main.c
@@ -1,67 +1,67 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS 1 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS 1 
-
-// Exemplo 1: Hello World! com PThreads
-#include <pthread.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-void *thread(void *vargp);
-
-int main()
-{
-	pthread_t tid;  // Estrutura que define uma thread
-	printf("Hello World da thread principal!\n");
-
-	// Cria uma thread com os atributos definidos em tid, opções padrão NULL,
-	// thread é função que contém o código da thread e não há parametros de entrada (ou seja, NULL)
-	pthread_create(&tid, NULL, thread, NULL);
-
-	// Espera a thread "tid" terminar e não captura seu valor de retorno retorno (NULL)
-	pthread_join(tid, NULL);
-
-	system("pause");
-
-	// Retorno NULL da thread principal. Desnecessário.
-	pthread_exit((void *)NULL);
-}
-
-void *thread(void *vargp)
-{
-	printf("Hello World da thread criada pela thread principal!\n");
-	pthread_exit((void *)NULL);
-}
-// --- Fim Exemplo 1 ---
-
-
-//// Exemplo 2: Escopo de variáveis
-//#include <stdio.h>
-//#include <stdlib.h>
+//
+//// Exemplo 1: Hello World! com PThreads
 //#include <pthread.h>
+//#include <stdlib.h>
+//#include <stdio.h>
 //
-//int global;
-//void *thr_func(void *arg);
+//void *thread(void *vargp);
 //
-//int main(void)
+//int main()
 //{
-//	pthread_t tid;
-//	global = 20;
-//	printf("Thread principal: %d\n", global);
-//	pthread_create(&tid, NULL, thr_func, NULL);
+//	pthread_t tid;  // Estrutura que define uma thread
+//	printf("Hello World da thread principal!\n");
+//
+//	// Cria uma thread com os atributos definidos em tid, opções padrão NULL,
+//	// thread é função que contém o código da thread e não há parametros de entrada (ou seja, NULL)
+//	pthread_create(&tid, NULL, thread, NULL);
+//
+//	// Espera a thread "tid" terminar e não captura seu valor de retorno retorno (NULL)
 //	pthread_join(tid, NULL);
-//	printf("Thread principal: %d\n", global);
+//
 //	system("pause");
-//	return 0;
+//
+//	// Retorno NULL da thread principal. Desnecessário.
+//	pthread_exit((void *)NULL);
 //}
 //
-//void *thr_func(void *arg)
+//void *thread(void *vargp)
 //{
-//	global = 40;
-//	printf("Novo thread: %d\n", global);
-//	return NULL;
+//	printf("Hello World da thread criada pela thread principal!\n");
+//	pthread_exit((void *)NULL);
 //}
-//// --- Fim Exemplo 2 ---
+//// --- Fim Exemplo 1 ---
+
+
+// Exemplo 2: Escopo de variáveis
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int global;
+void *thr_func(void *arg);
+
+int main(void)
+{
+	pthread_t tid;
+	global = 20;
+	printf("Thread principal: %d\n", global);
+	pthread_create(&tid, NULL, thr_func, NULL);
+	pthread_join(tid, NULL);
+	printf("Thread principal: %d\n", global);
+	system("pause");
+	return 0;
+}
+
+void *thr_func(void *arg)
+{
+	global = 40;
+	printf("Novo thread: %d\n", global);
+	return NULL;
+}
+// --- Fim Exemplo 2 ---
 
 //// --- Exemplo 3: Multiplas Threads com parâmetro
 //#include <stdio.h>
